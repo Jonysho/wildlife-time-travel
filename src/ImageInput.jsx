f048dc0ebf6ccc65dfd7895954df4f39c40b0b9e
jonathandw743 <jonathandw414@gmail.com>
2024-11-09 17:13:13 +0000

    wip

--- src/ImageInput.jsx
+++ src/ImageInput.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ApiInput = ({name}) => {
+const ApiInput = ({ name, apiState: [api, setApi] }) => {
     return <>
-            <input type="radio" name="api" id={`api-input${name}`} value={name} /><label htmlFor={`api-input-${name}`}>{name}</label>
+        <input type="radio" name="api" id={`api-input${name}`} value={name} checked={api == name} onChange={() => {
+            setApi(name);
+        }} /><label htmlFor={`api-input-${name}`}>{name}</label>
     </>
 }
 
 const ImageInput = () => {
-    const 
+    const [api, setApi] = useState("google-vision");
+    // const [file, setFile] = useState();
+    useEffect(() => {
+        console.log(file);
+    }, [file]);
     return (
         <div>
-            <input type="file" name="image" id="image-input" /><br />
-            <ApiInput name="google-vision"/><br />
-            <ApiInput name="plantnet"/><br />
-            <ApiInput name="plant-id"/><br />
+            <input type="file" name="image" id="image-input" onChange={(e) => {
+                const file = e.target.files[0];
+                if (file) {
+                    const reader = new FileReader();
+                    reader
+                }
+                console.log(e.target.result);
+                if (e.target.files.length > 1) {
+                    // assert("you can only upload one image");
+                }
+                setFile(e.target.files[0]);
+            }}/><br />
+            <ApiInput name="google-vision" apiState={[api, setApi]} /><br />
+            <ApiInput name="plantnet" apiState={[api, setApi]} /><br />
+            <ApiInput name="plant-id" apiState={[api, setApi]} /><br />
         </div>
     )
 }
