2fdb55beb0dd7b54d712676f4ceb2c0bf3e2f338
Jonathan <jon@Jonathans-MacBook-Pro-7.local>
2024-11-09 18:14:23 +0000

    Add all frontend components

---
+++ frontend/src/TimeSlider.jsx
@@ -0,0 +1,10 @@
+// TimeSlider
+const TimeSlider = () => {
+    return (
+        <div>
+            <input type="range" min="0" max="100" />
+        </div>
+    );
+};
+
+export default TimeSlider;
