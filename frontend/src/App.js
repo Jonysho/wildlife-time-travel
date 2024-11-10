6e8d690b9452bf0ea6746987eae07941fc44d61f
jonathandw743 <jonathandw414@gmail.com>
2024-11-10 05:24:16 +0000

    styles and stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Map from './Map';
 import ImageInput from './ImageInput';
 import Info from './Info';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
     const [data, setData] = useState({});
+    const [currentObjectIndex, setCurrentObjectIndex] = useState();
+    useEffect(() => {
+        // console.log(JSON.stringify(data));
+        console.log(data);
+    }, [data]);
     return (
         <div className="App" style={{ height: '100vh', width: '100vw', display: "flex", flexDirection: "row" }}>
-            <div style={{flex: "1 1 auto", display: "flex", flexDirection: "column", width: "100%"}}>
-            <ImageInput data={data} setData={setData}/>
-            <Info />
+            <div style={{flex: "1 1 auto", display: "flex", flexDirection: "column", width: "100%", maxWidth: "40rem"}}>
+            <ImageInput data={data} setData={setData} currentObjectIndex={currentObjectIndex} setCurrentObjectIndex={setCurrentObjectIndex}/>
+            <Info data={data} currentObjectIndex={currentObjectIndex}/>
             </div>
             <Map data={data}/>
         </div>
--- frontend/src/Info.jsx
+++ frontend/src/Info.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 
 // Info
-const Info = () => {
-    const [name, setName] = useState(null);
-    const [description, setDescription] = useState('');
+const Info = ({ data, currentObjectIndex }) => {
+    // const [name, setName] = useState(null);
+    // const [description, setDescription] = useState('');
 
     return (
-        <div style={{flex: "1 1 auto"}}>
+        <div className='vbox'>
             <h2>Animal/Plant Information</h2>
             <div>
-                <div>
-                    {name ? (
-                        <>
-                            <h3>{name}</h3>
-                            <p>{description}</p>
-                        </>
-                    ) : (
-                        <p>Please click on one of the objects</p>
-                    )}
-                </div>
+                {currentObjectIndex != undefined ? <>
+                    <p>Object Number: {currentObjectIndex}</p>
+                    <p>Identified as: {data.objects[currentObjectIndex].name}</p>
+                    <p>Kingdom: {data.objects[currentObjectIndex].species.kingdom}</p>
+                    <p>Phylum: {data.objects[currentObjectIndex].species.phylum}</p>
+                    <p>Order: {data.objects[currentObjectIndex].species.order}</p>
+                    <p>Family: {data.objects[currentObjectIndex].species.family}</p>
+                    <p>Genus: {data.objects[currentObjectIndex].species.genus}</p>
+                    <p>Species: {data.objects[currentObjectIndex].species.species}</p>
+                </> : <p>Please click on one of the objects</p>}
             </div>
         </div>
     );
