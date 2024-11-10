8fce00779149fe72fe7e3c98c857e4b9464672a2
jonathandw743 <jonathandw414@gmail.com>
2024-11-10 11:34:56 +0000

    better species detection

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from image_detection import detect_objects_in_image_base64
 from gbif import get_species
 from agent import answer_prompt
+import requests
 
 app = Flask(__name__)
 
@@ -15,6 +16,16 @@ def analyse_image():
 
     objects = detect_objects_in_image_base64(data["image"])
 
+    for o in objects:
+        res = requests.get(f"https://api.inaturalist.org/v1/taxa/autocomplete?q={o["name"]}")
+        r = res.json()["results"]
+        if len(r) > 0:
+            o["sci_name"] = r[0]["name"]
+            res2 = requests.get(f"https://api.gbif.org/v1/species/search?q={requests.utils.quote(o["sci_name"])}&limit=5")
+            r2 = res2.json()["results"]
+            if len(r2) > 0:
+                o["species"] = r2[0]
+
     objects.sort(key=lambda x: x["key"], reverse=True)
     # object = ["name": cat, "key": 1, "bounding_box": [(0.1, 0.1), (0.2, 0.2)]]
     seen_boxes=set()
@@ -23,7 +34,7 @@ def analyse_image():
     for o in objects:
         if o["bounding_box"][0] not in seen_boxes:
             if o["name"] not in seen_objects:
-                o["species"] = get_species(o["key"])
+                # o["species"] = get_species(o["key"])
                 prompt = """You are an expert in the field of biology with access to comprehensive databases 
                 and resources. Give me a description in 30 words max, understandable by people of all ages, for the object: """
                 o["desc"] = answer_prompt((prompt+o["name"]), o["species"])
